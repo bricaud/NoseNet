a16db3635452abc9489df2d2cdd194041a90b230
Benjamin Ricaud <benjamin.ricaud@gmail.com>
2021-04-22 15:07:22 +0200

    add config file

---
+++ cfg_nosenet.py
@@ -0,0 +1,8 @@
+params = {'DIM_EXPLOSION_FACTOR': 40,
+          'PROJECTION_TYPE': 'RK',
+          'NB_PROJ_ENTRIES': 6,
+          'HASH_LENGTH': 100,
+          'NB_FEATURES': None,
+          'NB_CLASSES': None,
+          'WTA': 'top'
+         }
