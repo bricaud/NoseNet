5c94892030f2936302c6c61a24e93f7474fc2924
Benjamin Ricaud <benjamin.ricaud@gmail.com>
2021-05-05 15:11:47 +0200

    add sparse_hebbian param

--- cfg_nosenet.py
+++ cfg_nosenet.py
@@ -1,8 +1,9 @@
 params = {'DIM_EXPLOSION_FACTOR': 20,
           'PROJECTION_TYPE': 'RK',
           'NB_PROJ_ENTRIES': 6,
-          'HASH_LENGTH': 400,
+          'HASH_LENGTH': 100,
           'NB_FEATURES': None,
           'NB_CLASSES': None,
-          'WTA': 'top'
+          'WTA': 'top',
+          'sparse_hebbian': False
          }
